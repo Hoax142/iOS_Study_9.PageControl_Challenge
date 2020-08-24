22739f8a45da7377e5595cde228899ae53594e33
Hoax142 <hyoh0606@gmail.com>
2020-08-24 16:15:49 +0900

    Initial Commit

---
+++ 9.PageControl_도전/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  9.PageControl_도전
+//
+//  Created by 오현호 on 2020/08/24.
+//  Copyright © 2020 hoax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 9.PageControl_도전Tests/__PageControl______Tests.swift
@@ -0,0 +1,34 @@
+//
+//  __PageControl______Tests.swift
+//  9.PageControl_도전Tests
+//
+//  Created by 오현호 on 2020/08/24.
+//  Copyright © 2020 hoax. All rights reserved.
+//
+
+import XCTest
+@testable import __PageControl______
+
+class __PageControl______Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
